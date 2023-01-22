a18ae1e756c3393d64dfd63fa7491156ea969e5a
David Haslip <103548776+davidhaslip@users.noreply.github.com>
2023-01-22 11:16:53 -0500

    update to support python 3

--- lambda_function.py
+++ lambda_function.py
@@ -9,12 +9,11 @@ def lambda_handler(event, context):
     sns_message = ast.literal_eval(event['Records'][0]['Sns']['Message'])
     target_bucket = context.function_name[:-1]
     source_bucket = str(sns_message['Records'][0]['s3']['bucket']['name'])
-    key = str(urllib.unquote_plus(sns_message['Records'][0]['s3']['object']['key']).decode('utf8'))
-    print "Deleteing %s from bucket %s ..." % (key, target_bucket)
+    key = str(urllib.parse.unquote_plus(sns_message['Records'][0]['s3']['object']['key']))
+    print(f"Deleteing {key} from bucket {target_bucket}")
     obj = s3.Object(target_bucket, key)
     try: 
         obj.load()
         obj.delete()
-    except:
-        print("Did not delete.")
-    
+    except Exception as e:
+        print("Did not delete because of error", e)
